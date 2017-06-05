38a8306b972d82c278e3ba4648149fac53dd6698
后居上 <test@123.com>
2017-06-05 17:05:27 +0800

    1111

--- src/page/sprider/index.vue
+++ src/page/sprider/index.vue
@@ -10,6 +10,11 @@
       </el-table-column>
       <el-table-column prop="addtime" label="添加时间">
       </el-table-column>
+      <el-table-column label="操作" width="100">
+        <template scope="scope">
+          <el-button type="text" size="small">发布</el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <el-pagination
       @size-change="pageSizeChange"
